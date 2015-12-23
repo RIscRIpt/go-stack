9963eb365bbaae0216f405db7a5242b081cab743
RIscRIpt <richard@riscript.com>
2015-12-23 20:09:17 +0200

    Add initial implementation of stack

---
+++ stack.go
@@ -0,0 +1,40 @@
+package stack
+
+type stack struct {
+	top  *element
+	size uint
+}
+
+type element struct {
+	value interface{}
+	next  *element
+}
+
+func New() *stack {
+	return &stack{}
+}
+
+func (s *stack) Size() uint { return s.size }
+
+func (s *stack) Push(value interface{}) {
+	s.top = &element{
+		value: value,
+		next:  s.top,
+	}
+	s.size++
+}
+
+func (s *stack) Top() (value interface{}) {
+	if s.size > 0 {
+		value = s.top.value
+	}
+	return
+}
+
+func (s *stack) Pop() (value interface{}) {
+	if s.size > 0 {
+		value, s.top = s.top.value, s.top.next
+		s.size--
+	}
+	return
+}
